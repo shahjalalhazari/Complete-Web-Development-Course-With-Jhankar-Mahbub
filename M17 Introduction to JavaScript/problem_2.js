36b73997951f6cdc01bf7fc91e639a3b38adb848
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2023-01-27 21:20:23 +0400

    Module 17 from Milestone 04.

---
+++ M17 Introduction to JavaScript/problem_2.js
@@ -0,0 +1,15 @@
+// Problem 3:- Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics and Bangla of a student.
+
+// Answere:-
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.35;
+var bangla = 99.50;
+
+var totalScore = mathematics + biology + chemistry + physics + bangla
+
+// Total number of subjects are 5
+var avgScore = totalScore / 5
+avgScore = avgScore.toFixed(2)
+console.log(avgScore)
---
+++ M17 Introduction to JavaScript/problem_4.js
@@ -0,0 +1,9 @@
+// Problem 4:- Sarah's mother is teaching her mathematics. She gave Sarah a number and asked her what the remainder would be if she divided the number by 5.
+
+// Answere:-
+var numberOfSarah = 119;
+var dividedBy = 5;
+
+var remaining = numberOfSarah % dividedBy
+
+console.log(remaining)
