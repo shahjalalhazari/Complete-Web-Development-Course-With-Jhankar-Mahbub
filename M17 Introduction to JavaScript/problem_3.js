02d1ee965deb7cf64ddec3a63329c9a6cdaca4c7
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2023-02-07 19:43:19 +0400

    M16 Assignment 03 from Milestone 3.

--- M17 Introduction to JavaScript/problem_3.js
+++
@@ -1,7 +0,0 @@
-// Problem 3:- John's tacher gave him two variables. Each variable contains a string. John's teacher asked him to combine these two strings and print them in one line.
-
-// Answere:-
-var string1 = "I am going to be";
-var string2 = "an awesome web developer."
-
-console.log(string1 + " " + string2)
