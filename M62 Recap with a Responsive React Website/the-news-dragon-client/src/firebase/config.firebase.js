0a21c28f602bb4f24cd965e47b3ba09c6b1ff1cd
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2023-04-24 22:27:38 +0400

    Module 62 from Milestone 10 'the-news-dragon-client' project setup done

---
+++ M62 Recap with a Responsive React Website/the-news-dragon-client/src/firebase/config.firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import {
+    initializeApp
+} from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBM-s0c6CbVl5mjiXkLT6vBEq4wrHBtXMo",
+    authDomain: "the-news-dragon-3372c.firebaseapp.com",
+    projectId: "the-news-dragon-3372c",
+    storageBucket: "the-news-dragon-3372c.appspot.com",
+    messagingSenderId: "316268700738",
+    appId: "1:316268700738:web:d4d158be0a4339f003f9db"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
